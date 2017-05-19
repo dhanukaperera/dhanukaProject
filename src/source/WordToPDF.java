fc4058caca1ea7ea40a075e9b46e2fd20e096d44
dhanukaperera <dhanukashehanperera@gmail.com>
2017-05-19 12:41:52 +0530

    removing docs

--- src/source/WordToPDF.java
+++ src/source/WordToPDF.java
@@ -5,36 +5,54 @@
  */
 package source;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  *
  * @author rpa09
  */
 public class WordToPDF {
-    
+
     final static String SCRIPTPATH = "src\\script\\ConvertWordToPDF.vbs";
-    public static void main(String[] args)
-    {
-        
+
+    public static void main(String[] args) {
+
         //String vbScriptPATH = "src\\script\\ConvertWordToPDF.vbs";
-        String docPATH="C:\\javaTest";
+        String docPATH = "C:\\javaTest";
         Convert(docPATH);
-        
+
     }
-    
-    private static String SRCIPTPATH()
-    {
+
+    private static String SRCIPTPATH() {
         return SCRIPTPATH;
     }
-    
+
     public static void Convert(String pdocPATH) {
         try {
             Runtime.getRuntime().exec("wscript " +"\""+SRCIPTPATH()+"\""+ " " +"\""+pdocPATH+"\"");
+           removeDocs();
         } catch (IOException e) {
             System.out.println(e);
             System.exit(0);
         }
 
+    }
+
+    public static void removeDocs() throws IOException {
+        ProcessBuilder builder = new ProcessBuilder(
+                "cmd.exe", "/c", "cd \"C:\\javaTest\" &&  del *.doc");
+        builder.redirectErrorStream(true);
+        Process p = builder.start();
+        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line;
+        while (true) {
+            line = r.readLine();
+            if (line == null) {
+                break;
+            }
+            System.out.println(line);
+        }
     }
 }
