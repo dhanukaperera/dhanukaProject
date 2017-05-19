f98ba70bd512c263f38e80699de8edc46cac5709
dhanukaperera <dhanukashehanperera@gmail.com>
2017-05-19 12:03:51 +0530

    updates

---
+++ src/source/WordToPDF.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package source;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author rpa09
+ */
+public class WordToPDF {
+    
+    final static String SCRIPTPATH = "src\\script\\ConvertWordToPDF.vbs";
+    public static void main(String[] args)
+    {
+        
+        //String vbScriptPATH = "src\\script\\ConvertWordToPDF.vbs";
+        String docPATH="C:\\javaTest";
+        Convert(docPATH);
+        
+    }
+    
+    private static String SRCIPTPATH()
+    {
+        return SCRIPTPATH;
+    }
+    
+    public static void Convert(String pdocPATH) {
+        try {
+            Runtime.getRuntime().exec("wscript " +"\""+SRCIPTPATH()+"\""+ " " +"\""+pdocPATH+"\"");
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+
+    }
+}
